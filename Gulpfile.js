42f73cf507c248eba4c4786c1c06e6c9c22d073c
clinkenb <ken-clinkenbeard@uiowa.edu>
2024-09-28 21:12:57 -0500

    Add gulp and node.js to workflow

---
+++ Gulpfile.js
@@ -0,0 +1,39 @@
+const { src, dest, series, watch } = require('gulp');
+
+// styles
+const sass = require('gulp-sass')(require('sass'));
+const autoPrefixer = require('gulp-autoprefixer');
+const cleanCSS = require('gulp-clean-css');
+const sourcemaps = require('gulp-sourcemaps');
+const cache = require('gulp-cached');
+const dependents = require('gulp-dependents');
+const size = require('gulp-size');
+
+function stylesTask() {
+  const stylesSource = './sass/**/*.scss';
+  const stylesDest1 = './css/';
+  const stylesDest2 = './_includes/';
+
+  // const sizefull = size({ showFiles: true, uncompressed: true });
+  const sizegzip = size({ showFiles: true, gzip: true });
+
+  return src(stylesSource)
+    .pipe(cache('sasscache'))
+    .pipe(dependents())
+    .pipe(sourcemaps.init())
+    .pipe(sass())
+    .pipe(autoPrefixer({ grid: false }))
+    .pipe(cleanCSS({ format: 'compress' }))
+    // .pipe(sizefull)
+    .pipe(sizegzip)
+    .pipe(sourcemaps.write())
+    .pipe(dest(stylesDest1))
+    .pipe(dest(stylesDest2));
+}
+
+// watch
+function watchTask() {
+  watch(['./sass/**/*.scss'], series(stylesTask))
+}
+
+exports.default = series(stylesTask, watchTask);
